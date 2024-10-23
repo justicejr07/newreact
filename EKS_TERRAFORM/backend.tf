50f7bde662866c71b44aeea1dffdd990396413f7
justicejr07 <106585765+justicejr07@users.noreply.github.com>
2024-10-23 10:24:00 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "akhotstarbucket-akas" # Replace with your actual S3 bucket name
+    bucket = "ahotstarbucket-justice" # Replace with your actual S3 bucket name
     key    = "EC2/terraform.tfstate"
     region = "us-east-1"
   }
